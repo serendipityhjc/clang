6ceca4c261a0066c94901aa62e371fbb7e163c44
ITSC <itsc@example.com>
2023-07-08 15:41:46 +0900

    ex04

--- ex04-02-scanf.c
+++ ex04-02-scanf.c
@@ -10,6 +10,7 @@ scanf()
 
 int main(void)
 {
+    /*
     char character;
     int inum;
     float fnum;
@@ -19,6 +20,29 @@ int main(void)
     scanf("%f", &fnum);
 
     printf("%c, %d, %f \n", character, inum, fnum);
+    */
+
+   /*
+   int num1, num2, num3;
+   float fnum1, fnum2;
+   printf("세 개의 정수 입력: ");
+   scanf("%d %d %d", &num1, &num2, &num3);
+   printf("입력된 정수들: %d %d %d \n", num1, num2, num3);
+   */
+
+    /*
+    int num1, num2, num3;
+    float fnum1, fnum2;
+    printf("실수, 정수, 실수 차례대로 입력: ");
+    scanf("%f %d %f", &fnum1, &num1, &fnum2);
+    printf("입력된 값들: %f %d %f", fnum1, num1, fnum2);
+    */
+
+    int num1, num2, num3;
+    float fnum1, fnum2;
+    printf("세 개의 정수 입력: ");
+    scanf("%o %x %d", &num1, &num2, &num3);
+    printf("입력된 값들: %d %d %d", num1, num2, num3);
 
     return 0;
 }

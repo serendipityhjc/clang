fea461345a841a3bda1bff455670e04ee4854b9a
ITSC <itsc@example.com>
2023-07-09 16:00:19 +0900

    ex08-99

--- ex08-01-loop.c
+++ ex08-01-loop.c
@@ -11,18 +11,32 @@
     }
 
 */
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 int main(void)
 {
-    int num = 0;
+    // int num = 0;
 
-    while(num < 3)
+    // while(num < 3)
+    // {
+    //     printf("num is %d now\n", num);
+    //     num++;  // 후위 증감연산자 num = num +1 동일
+    // }
+
+    // printf("End while문 - num is %d now\n", num);
+
+    int num2 = 0;
+    while (num2 != -1)
     {
-        printf("num is %d now\n", num);
-        num++;  // 후위 증감연산자 num = num +1 동일
-    }
+        printf("숫자를 입력하세요(-1 입력시 종료): ");
+        scanf("%d", &num2);
 
-    printf("End while문 - num is %d now\n", num);
+        if(num2 == -1)
+            printf("종료합니다.\n");
+        else
+            printf("%d을(를) 입력하셨네요.\n", num2);
+            
+    }
 
     return 0;
 }

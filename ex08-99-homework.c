2f57d96facd6e90c26258b6e31d575875f57b867
ITSC <itsc@example.com>
2023-07-15 14:31:01 +0900

    230715 수업

--- ex08-99-homework.c
+++ ex08-99-homework.c
@@ -32,20 +32,51 @@
    *
 
 */
+
+//#include <stdio.h>
+
+// int main(void)
+// {
+
+//     for (int i = 0; i < 7; i++)
+//     {
+        
+//         for (int j = 0; j < i+1; j++)
+//         {
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
+
+
+
+//4번문제 풀이
 #include <stdio.h>
 
 int main(void)
 {
-
-    for (int i = 0; i < 7; i++)
+  for (int i = 0; i < 7; i++)
+  {
+    for(int j = 0; j <7; j++)
     {
-        
-        for (int j = 0; j < i+1; j++)
-        {
-            printf("*");
-        }
-        printf("\n");
-    }
+        if(i < 7/2+1) {
+            if(j < 3-i) {
+                printf(" ");
+            } else if(j > 3+i) {
+                printf(" ");
+            } else {
+                printf("*");
+            }
 
-    return 0;
+        } else {
+        printf("*");
+
+        }
+    }
+    printf("\n");
+    
+  }  
 }

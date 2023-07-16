10f6451f8c00cdc6c2d2ebc1b8e9487ceb5dd716
ITSC <itsc@example.com>
2023-07-16 16:09:27 +0900

    230716 수업

--- ex15-04-function4.c
+++ ex15-04-function4.c
@@ -10,9 +10,27 @@ void guide(void)
     printf("또한 printNumber 함수를 통해 입력한 정수를 출력할 수도 있습니다.\n");
 }
 
+// 입력값만 존재하는 경우
+void printNumber(int num)
+{
+    printf("당신이 입력한 정수는 %d입니다.\n", num);
+}
+
+// 반환값만 존재하는 경우
+int inputNumber(void)
+{
+    int num;
+    printf("정수를 입력해주세요: ");
+    scanf("%d", &num);
+
+    return num;
+}
+
 int main(void)
 {
     guide();
+    int num = inputNumber();    // 정수형 값 입력
+    printNumber(5);
 
     return 0;
 }
